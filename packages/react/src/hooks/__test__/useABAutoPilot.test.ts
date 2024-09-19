12816f185909822174400df959743822d2df8182
Will <fowlerwill@gmail.com>
2024-09-18 21:10:04 -0600

    added test

--- packages/react/src/hooks/__test__/useABAutoPilot.test.ts
+++ packages/react/src/hooks/__test__/useABAutoPilot.test.ts
@@ -1,12 +1,29 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import { renderHook } from "@testing-library/react";
 import useABAutoPilot from "../useABAutoPilot";
 
 describe("useABAutoPilot", () => {
   it("should return the correct values", () => {
     const expectedTeam = 1;
+    const expectedCopy = "boo!";
     const experiment = "experiment";
     const token = "token";
+
+    // must mock fetch
+    global.fetch = vi.fn(() =>
+      Promise.resolve({
+        json: () =>
+          Promise.resolve([
+            {
+              id: 1,
+              copy: expectedCopy,
+              seenCount: 0,
+              clickedCount: 0,
+            },
+          ]),
+      }),
+    );
+
     const { result } = renderHook(() =>
       useABAutoPilot(expectedTeam, experiment, token),
     );
